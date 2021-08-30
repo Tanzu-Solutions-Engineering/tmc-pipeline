79c696fde17a5a6045923e525230431e1e78cfad
Dave Roberts <droberts@droberts-a02.vmware.com>
2021-08-30 14:15:56 -0500

    adding script

---
+++ script/update-cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tmc cluster create -f cluster.yaml
+
+if [ $? -eq 0 ]
+then
+  echo "cluster create command successfully executed"
+  exit 0
+else
+  echo "cluster create command failed.  Trying upgrade"
+  tmc cluster update -f cluster.yaml
+fi
+
+if [ $? -ne 0 ]
+then
+  echo "cluster upgrade command failed"
+else
+  echo "cluster upgrade command successfully executed"  
+fi
